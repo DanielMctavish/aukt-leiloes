f295aa606814a7324f9bb5105272826e47665c70
DanielMctavish <dc.uneb@gmail.com>
2023-10-06 10:20:18 -0300

    AUKT-first

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App flex flex-col justify-center items-center h-[100vh] gap-3 bg-zinc-100">
+      <span className="text-zinc-600">tudo começa por aqui</span>
+      <button className="text-zinc-400">
+        <a href="https://dmdesenvolvedor.com.br" target="_blank" rel="noreferrer">Arboris Codex_</a>
+      </button>
+    </div>
+  );
+}
+
+export default App;
