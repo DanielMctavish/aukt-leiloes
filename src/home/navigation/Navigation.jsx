a3080109f8b599e473cd395d77ddf7c6bd0ff541
DanielMctavish <danielcarlos_@live.com>
2023-10-10 13:47:14 -0300

    finish navigation

---
+++ src/home/navigation/Navigation.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react"
+import { Home, Help, People, EventAvailable, Category, AccountCircle, Menu } from "@mui/icons-material"
+
+function Navigation() {
+    const [navigationMenu,setNavigationMenu] = useState(false)
+
+    const tailwindItems = "flex justify-center items-center p-2 gap-3"
+   
+    const hideNavigation = ()=>{
+
+        const navigationAuk = document.querySelector(".nav-auk")
+        
+        if(!navigationMenu){
+            navigationAuk.style.marginLeft = '-40vh'
+            navigationAuk.style.transition = '.6s'
+            setNavigationMenu(!navigationMenu)
+        }else{
+            navigationAuk.style.marginLeft = '0vh'
+            navigationAuk.style.transition = '.2s'
+            setNavigationMenu(!navigationMenu)
+        }
+
+    }
+
+    return (
+        <nav className="
+        nav-auk
+        lg:left-auto
+        left-0
+        lg:relative
+        fixed
+        lg:w-full
+        w-[220px] 
+        lg:h-[4vh]
+        h-[100vh] 
+        bg-[#012038] 
+        lg:flex-col
+        flex-row
+        flex 
+        justify-center
+        lg:justify-start 
+        items-center 
+        gap-2 
+        text-white 
+        text-[12px]
+        cursor-pointer">
+
+            <span 
+            style={{fontSize:'26px'}}
+            className="flex 
+            fixed 
+            top-1 
+            right-1 
+            lg:hidden 
+            text-[#012038] 
+            bg-white 
+            p-2 
+            rounded-md 
+            z-40 
+            w-[44px] 
+            h-[44px] 
+            justify-center
+            items-center"
+            onClick={hideNavigation}>
+                <Menu/>
+            </span>
+
+            <div className="
+            lg:w-[70%] 
+            w-full 
+            lg:h-[4vh]
+            h-[100vh] 
+            flex 
+            flex-col 
+            lg:flex-row 
+            justify-between 
+            items-center">
+
+                <section className="
+                lg:mt-0
+                mt-[8vh]
+                h-[42px] 
+                lg:w-[60%]
+                w-full
+                lg:flex-row
+                flex-col
+                flex 
+                justify-between 
+                items-center 
+                gap-2 
+                text-white">
+                    <button className={tailwindItems}>
+                        <Home />
+                        HOME
+                    </button>
+                    <button className={tailwindItems}>
+                        <People />
+                        QUEM SOMOS
+                    </button>
+                    <button className={tailwindItems}>
+                        <Help />
+                        AJUDA
+                    </button>
+                    <button className={tailwindItems}>
+                        <EventAvailable />
+                        AGENDA
+                    </button>
+                    <button className={tailwindItems}>
+                        <Category />
+                        PRODUTOS
+                    </button>
+                </section>
+
+                <section>
+                    <button className={tailwindItems}>
+                        <AccountCircle />
+                        ANUNCIAR
+                    </button>
+                </section>
+
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navigation
