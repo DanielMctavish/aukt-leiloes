71a980219d685b02c22c90b69ce55f3eaa3e1452
Katianne23 <katianne40@gmail.com>
2023-10-30 16:24:22 -0300

    Footer

--- src/home/components/Footer.jsx
+++ src/home/components/Footer.jsx
@@ -1,10 +1,159 @@
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
-
-function Footer () {
-    return (
-        <section className="w-[1920px] h-[900px] border-t-100 border-[#1E1E1E] bg-[#012038]"></section>
-    )
+function Footer() {
+  return (
+    <section className="sm:w-[1520px] sm:h-[820px] h-full border-t-[100px] border-[#1E1E1E] bg-[#012038] overflow-hidden">
+      <div className="items-center justify-center p-[20px]">
+        <div className="mt-[10px]">
+          <span className="text-[#FFFFFF1F] text-[77px] pl-10 font-bold leading-[30.415px]">
+            A U K T
+          </span>
+        </div>
+        <div className="flex items-center justify-center">
+          <span className="text-[#FFFFFF08] text-[77px] pl-10 font-bold leading-[30.415px]">
+            A U K T
+          </span>
+        </div>
+        <div className="flex items-center justify-center sm:ml-[100px] sm:p-[90px] flex-wrap">
+          <div className="w-1/2 sm:w-1/4 p-4">
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Podemos Ajudar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Quem somos?
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              AUKT informacional
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso lugar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Política de privacidade
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Termos e condições
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso blog
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Cadastre-se
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Acesso anunciante
+            </span>
+          </div>
+          <div className="w-1/2 sm:w-1/4 p-4">
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Podemos Ajudar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Quem somos?
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              AUKT informacional
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso lugar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Política de privacidade
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Termos e condições
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso blog
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Cadastre-se
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Acesso anunciante
+            </span>
+          </div>
+          <div className="w-1/2 sm:w-1/4 p-4">
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Podemos Ajudar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Quem somos?
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              AUKT informacional
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso lugar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Política de privacidade
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Termos e condições
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso blog
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Cadastre-se
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Acesso anunciante
+            </span>
+          </div>
+          <div className="w-1/2 sm:w-1/4 p-4">
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Podemos Ajudar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Quem somos?
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              AUKT informacional
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso lugar
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Política de privacidade
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Termos e condições
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Nosso blog
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Cadastre-se
+            </span>
+            <span className="text-[#fff] text-[16px] block mb-2">
+              Acesso anunciante
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="ml-[100px]">
+        <span className="text-[#FFFFFF1F] text-[48px] font-bold leading-[30.415px]">
+          A U K T
+        </span>
+      </div>
+      <div className="flex items-center justify-end">
+        <span className="text-[#FFFFFF08] text-[122px] font-bold leading-[30.415px]">
+          A U K 
+        </span>
+        <a
+          href="https://www.instagram.com/seuinstagram"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4"
+        >
+          <InstagramIcon />
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
