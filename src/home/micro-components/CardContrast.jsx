016f5a35161ed96fac1e918a6db33b1480803d4a
Katianne23 <katianne40@gmail.com>
2023-11-04 11:14:23 -0300

    Card section 02

--- src/home/micro-components/CardContrast.jsx
+++ src/home/micro-components/CardContrast.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function CardContrast(props) {
   return (
-    <div class="w-[361px] h-[337px] flex-shrink-0 border-2 items-center justify-center pt-4 border-gray-300 rounded-[2px] bg-[#EEEEEE]">
+    <div  key={props.index} class="w-[361px] h-[337px] flex-shrink-0 border-2 items-center justify-center pt-4 border-gray-300 rounded-[2px] bg-[#EEEEEE]">
       <span className="w-[270px] h-[213px]">
-        <img src={props.imagem} alt="" className="object-cover pl-10" />
+        <img src={props.card.imagem} alt="" className="object-cover pl-10" />
       </span>
       <span className="text-[#003A67] text-[14px] leading-[0.28px] font-normal block pl-6 pt-7 ">
-        {props.title}
+        {props.card.title}
       </span>
       <span className="text-[#949494] text-[12px] font-normal leading-[0.24px] pl-6 pt-7">
-        {props.body}
+        {props.card.body}
       </span>
       <div className="w-[68px] h-[15px] bg-[#F00] rounded-[2px] flex items-center justify-end ml-auto mr-4 mt-7">
         <span className="text-[#FFF] text-[12px] font-bold leading-[0.24px] p-2">
