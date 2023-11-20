ed9c4643c6cb1eccd9deeab34a0868a55ea4f991
DanielMctavish <danielcarlos_@live.com>
2023-11-20 12:55:27 -0300

    changes-home

--- src/home/components/Section01.jsx
+++ src/home/components/Section01.jsx
@@ -26,10 +26,10 @@ function Section01() {
 
     { title: 'primeiro card1', body: 'conteúdo do card1', urlCover: 'https://cdn.motor1.com/images/mgl/Znnm7r/s1/ferrari-sp48-unica.webp' },
     { title: 'primeiro card2', body: 'conteúdo do card2', urlCover: 'https://fotos-jornaldocarro-estadao.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2022/09/19125632/Ferrari-Purosangue-2.jpg' },
-    { title: 'primeiro card3', body: 'conteúdo do card3', urlCover: 'https://img.olhardigital.com.br/wp-content/uploads/2023/07/barbie-661761_728-410-0-0.jpg' },
+    { title: 'primeiro card3', body: 'conteúdo do card3', urlCover: 'https://m.media-amazon.com/images/I/81meftoQVJL._AC_UF894,1000_QL80_.jpg' },
     { title: 'primeiro card4', body: 'conteúdo do card4', urlCover: 'https://m.media-amazon.com/images/I/71hdwN+riFL._AC_UF1000,1000_QL80_.jpg' },
     { title: 'primeiro card5', body: 'conteúdo do card5', urlCover: 'https://a-static.mlcdn.com.br/450x450/kit-4-beyblade-burst-com-lancador-rantaro-daigo-shu-e-outros-lianfa-toys/sandrapresentes/131987245p/a9d09d53d21ca135b055fd62879ce28a.jpeg' }
-    
+
   ]
 
   return (
