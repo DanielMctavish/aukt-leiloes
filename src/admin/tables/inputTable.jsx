b600d1d2ee0abe05955e49c820ca898eb72f38d1
Katianne23 <katianne40@gmail.com>
2023-12-05 11:52:27 -0300

    aucts Responsive

--- src/admin/tables/inputTable.jsx
+++ src/admin/tables/inputTable.jsx
@@ -2,10 +2,10 @@ import SearchIcon from "@mui/icons-material/Search";
 
 function inputTable() {
   return (
-    <tr className="border-b-[.4px] border-zinc-300">
-      <th className="px-6 py-3 text-zinc-400 font-semibold flex justify-end items-center">
+    <tr className="border-b-[.4px] border-zinc-300 ">
+      <th className="px-6 py-3 lg:mt-0 mt-6 text-zinc-400 font-semibold flex lg:justify-end lg:items-center">
         <div className="relative">
-          <input className="bg-[#D7DDE7] rounded-[4px] pl-4 pr-2 p-1" />
+          <input className="lg:bg-[#D7DDE7] bg-[#96A3BA] rounded-[4px] lg:w-full w-[320px] pl-4 pr-2 p-1" />
           <SearchIcon className="absolute top-1 left-2 text-[#FFFFFF]" />
         </div>
       </th>
