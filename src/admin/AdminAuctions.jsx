7d7d4f556597b2cad90e072613f7bbd656a91fe9
Katianne23 <katianne40@gmail.com>
2023-12-08 11:01:33 -0300

    Ajuste Scroll

--- src/admin/AdminAuctions.jsx
+++ src/admin/AdminAuctions.jsx
@@ -6,19 +6,19 @@ import InputTable from "./tables/inputTable";
 function AdminAuctions() {
   return (
     <div
-      className="w-full lg:h-[100vh] h-auto 
-    bg-[#D8DEE8] text-zinc-600 overflow-hidden
+      className="w-full lg:h-[100vh] h-auto
+    bg-[#D8DEE8] text-zinc-600  overflow-x-hidden custom-scrollbar
      flex lg:flex-row flex-col justify-start items-start"
     >
       <AssideAdmin MenuSelected="menu-2" />
       <section className="w-full h-[100vh] flex flex-col justify-start items-center">
         <NavAdmin />
 
-        <section className="w-[90%] pt-6 flex lg:justify-center lg:items-center lg:gap-6 lg:flex-row flex-col">
+        <section className="w-[90%] h-full pt-6 flex lg:justify-center lg:items-center lg:gap-6 lg:flex-row flex-col">
           <div
-            className="w-full bg-[#D8DEE8] lg:bg-white  rounded-md 
+            className="w-full bg-[#D8DEE8] lg:bg-white rounded-md 
                     lg:shadow-lg lg:shadow-[#17171722] flex 
-                    flex-col overflow-x-hidden"
+                    flex-col"
           >
             <InputTable />
             <div className="py-2">
