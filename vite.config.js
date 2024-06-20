69efcd9dba4cb43a8adfe544233c7fa9395d95e2
Daniel  Arruda <66955006+DanielMctavish@users.noreply.github.com>
2024-06-20 16:25:16 -0300

    change vite backend url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:3009',
+        target: 'https://api.appaukserver.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
