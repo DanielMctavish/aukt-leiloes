a6752a989e56bb49a8656a293f469286595d9817
DanielMctavish <dc.uneb@gmail.com>
2024-06-20 13:30:14 -0300

    update enviroment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3032,
+    port: 3001,
     proxy: {
       '/api': {
         target: 'http://localhost:3009',
