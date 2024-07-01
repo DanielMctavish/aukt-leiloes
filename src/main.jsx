8e7e53a52aa434c43b02714a2ca46d06e6b482c8
DanielMctavish <dc.uneb@gmail.com>
2024-07-01 00:44:47 -0300

    open frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 import store from "./features/Store.js";
 import { Provider } from "react-redux"
 import './index.css'
-import DeveloperScreen from './DeveloperScreen.jsx';
+// import DeveloperScreen from './DeveloperScreen.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <Provider store={store}>
-      <DeveloperScreen />
-      {/* <App /> */}
+      {/* <DeveloperScreen /> */}
+      <App />
     </Provider>
 
-  </React.StrictMode>,
+  </React.StrictMode>
 )
