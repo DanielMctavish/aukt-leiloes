0957364f468f02173a82e0fcf61ed002e6086a1c
DanielMctavish <dc.uneb@gmail.com>
2024-12-11 11:37:07 -0300

    fixed-bid

--- src/advertiser/control/components/BidsListController.jsx
+++ src/advertiser/control/components/BidsListController.jsx
@@ -1,77 +1,63 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState, useCallback, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState, useRef } from "react";
+import { useSelector } from "react-redux";
 import io from 'socket.io-client';
 import BidDetailController from "./BidDetailController";
 import ClientBidDetailsController from "./ClientBidDetailsController";
-import { setCurrentProduct } from "../../../features/auct/generalAUKSlice";
 
 function BidsListController() {
-    const [localBids, setLocalBids] = useState([]);
+    const [allBids, setAllBids] = useState([]);
     const [selectedBid, setSelectedBid] = useState(null);
+    // const [currentProduct, setCurrentProduct] = useState(null);
     const generalAUK = useSelector(state => state.generalAUK);
-    const dispatch = useDispatch();
-    const newBidsRef = useRef([]);
+    const socketRef = useRef(null);
 
-    const updateBids = useCallback((newBid) => {
-        newBidsRef.current = [newBid, ...newBidsRef.current];
-        
-        setLocalBids(prevBids => {
-            const updatedBids = [newBid, ...prevBids];
-            // Remove duplicates based on bid ID or another unique identifier
-            return updatedBids.filter((bid, index, self) =>
-                index === self.findIndex((t) => t.id === bid.id)
-            );
-        });
-        
-        if (generalAUK.currentProduct) {
-            const updatedProduct = {
-                ...generalAUK.currentProduct,
-                Bid: [newBid, ...(generalAUK.currentProduct.Bid || [])]
-            };
-            dispatch(setCurrentProduct(updatedProduct));
-        }
-    }, [generalAUK.currentProduct, dispatch]);
-
+    // Atualiza o produto atual quando o Redux mudar (de 1 em 1 segundo)
     useEffect(() => {
-        let socket;
-        if (generalAUK.auct && generalAUK.status === 'live') {
-            socket = io(`${import.meta.env.VITE_APP_BACKEND_WEBSOCKET}`);
+        if (generalAUK.currentProduct?.id) {
+            
+            if (generalAUK.currentProduct.Bid && allBids.length === 0) {
+                setAllBids(generalAUK.currentProduct.Bid);
+            }
 
-            socket.on(`${generalAUK.auct.id}-bid`, (message) => {
+        }
+    }, [generalAUK.currentProduct]);
 
-                if (message && message.data) {
-                    updateBids(message.data);
+    // Configurar websocket apenas quando tivermos o produto atual
+    useEffect(() => {
+        const setupSocket = () => {
+            if (socketRef.current) {
+                socketRef.current.disconnect();
+            }
+
+            socketRef.current = io(`${import.meta.env.VITE_APP_BACKEND_WEBSOCKET}`);
+
+            socketRef.current.on(`${generalAUK.auct.id}-bid`, async (message) => {
+                try {
+                    setAllBids(message.data.body.Product[0].Bid)
+                } catch (error) {
+                    // console.error("Erro ao buscar lances:", error);
                 }
             });
 
-            socket.on(`${generalAUK.auct.id}-auct-finished`, () => {
+            socketRef.current.on(`${generalAUK.auct.id}-auct-finished`, () => {
                 // Adicione aqui a lógica para lidar com o fim do leilão
             });
+        };
+
+        if (generalAUK.auct && generalAUK.status === 'live') {
+            // console.log("Configurando socket com produto:", currentProduct);
+            setupSocket();
         }
 
         return () => {
-            if (socket) {
-                socket.disconnect();
+            if (socketRef.current) {
+                socketRef.current.disconnect();
+                socketRef.current = null;
             }
         };
-    }, [generalAUK.auct, generalAUK.status, updateBids]);
+    }, [generalAUK.auct, generalAUK.status]);
 
-    useEffect(() => {
-        // Atualizar localBids quando currentProduct mudar
-        if (generalAUK.currentProduct && generalAUK.currentProduct.Bid) {
-            setLocalBids(prevBids => {
-                const combinedBids = [...newBidsRef.current, ...generalAUK.currentProduct.Bid];
-                // Remove duplicates and sort by timestamp (assuming there's a timestamp field)
-                const uniqueBids = combinedBids.filter((bid, index, self) =>
-                    index === self.findIndex((t) => t.id === bid.id)
-                ).sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-                
-                newBidsRef.current = []; // Clear the new bids reference
-                return uniqueBids;
-            });
-        }
-    }, [generalAUK.currentProduct]);
 
     const handleBidClick = (bid) => {
         setSelectedBid(bid);
@@ -82,7 +68,11 @@ function BidsListController() {
     };
 
     if (!generalAUK.auct || generalAUK.status !== 'live') {
-        return <div className="flex items-center justify-center h-full">Nenhum leilão em andamento</div>;
+        return (
+            <div className="flex justify-center items-center lg:w-[50%] w-full h-full bg-white p-3 rounded-md">
+                Nenhum leilão em andamento
+            </div>
+        )
     }
 
     return (
@@ -91,15 +81,21 @@ function BidsListController() {
                 <span className="flex justify-start items-center w-full h-[46px] bg-[#012038] text-white p-2 font-bold text-[14px]">
                     Listagem de lances - Lote {generalAUK.currentProduct?.lote || "N/A"}
                 </span>
-                <div className="flex flex-col w-full h-[80%] overflow-y-auto p-2">
-                    {localBids.length > 0 ? (
-                        localBids.map((bid, index) => (
+                <div className="flex flex-col w-full h-[80%] overflow-y-auto p-2 gap-2">
+                    {allBids?.length > 0 ? (
+                        allBids.map((bid, index) => (
                             bid && bid.Client ? (
-                                <div key={bid.id || index} onClick={() => handleBidClick(bid)}>
+                                <div
+                                    key={bid.id || index}
+                                    onClick={() => handleBidClick(bid)}
+                                    className="cursor-pointer hover:opacity-90 transition-opacity"
+                                >
                                     <BidDetailController bid={bid} />
                                 </div>
                             ) : (
-                                <div key={index}>Invalid bid data</div>
+                                <div key={index} className="p-2 bg-red-50 text-red-600 rounded">
+                                    Dados do lance inválidos
+                                </div>
                             )
                         ))
                     ) : (
@@ -108,7 +104,9 @@ function BidsListController() {
                 </div>
             </div>
             {selectedBid && (
-                <ClientBidDetailsController bid={selectedBid} onClose={handleCloseModal} />
+                <ClientBidDetailsController
+                    bid={selectedBid}
+                    onClose={handleCloseModal} />
             )}
         </div>
     );
